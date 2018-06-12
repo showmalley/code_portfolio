cbc3cc410ba36ef5aa9a6d78dec0ed9bf60a6be7
Sean O'Malley <SeanOMalley1@Seans-MacBook-Pro-2.local>
2018-06-12 11:12:39 -0600

    uni, bi and trip gram kpi valuation completed

--- Natural Language Processing/TweetValuation.py
+++ Natural Language Processing/TweetValuation.py
@@ -24,14 +24,14 @@ import nltk
 import re
 from nltk.stem import PorterStemmer
 from nltk.tokenize import WordPunctTokenizer
+from nltk import bigrams as bi_grams
+from nltk import trigrams as tri_grams
 
 # Import  
 
 tweet_df = pd.read_csv('TweetResponseSample.csv')
 
-############################
-
-# Tokenizing Function
+############################ Tokenizing Function
 
 esw = nltk.corpus.stopwords.words('english') #i think we want to keep stopwords here
 word_pattern = re.compile("^\w+$")
@@ -41,9 +41,7 @@ def get_text(text):
     tokens = [token for token in tokens if re.match(word_pattern, token)]
     return(tokens)
     
-############################
-    
-# Get Unigram Values by client for each 
+############################ Unigram
 
 def unigram_vals(tweet_df):
         
@@ -71,7 +69,9 @@ def unigram_vals(tweet_df):
         
         # dynamically filter to business
         
-        dfx = df[df['business_name'].str.contains(business_names[i])].reset_index() # make i
+        dfx = df[df['business_name'].str.contains(business_names[i])].reset_index() 
+        
+        print("The length of " + str(dfx['business_name'].unique()) + " is " + str(len(dfx)))
     
         # tokenize each tweet
         
@@ -105,27 +105,187 @@ def unigram_vals(tweet_df):
         
         dfx = dfx[dfx['count'] >= 5].sort_values(by=['count','value'],ascending = False)
         
-        print("Client " + str(i + 1) + " is completed")
+        print(str(business_names[i]) + " is completed")
         
         df_final = df_final.append(dfx)
         
-        print(df_final.head())
+        print(dfx.head())
         
     return df_final
 
-############################
+############################ Bigram
 
-# Get Bigram Values by client for each 
+def bigram_vals(tweet_df):
+        
+    '''prerequisite functions: get_text'''
+    
+    # Read in just what we want
+    
+    df = tweet_df.loc[:,['business_name','conversations_status','tweet_message']]
+    
+    # Convert conversations status to binary
+    
+    df['conversations_status'] = np.where(df['conversations_status'] == 'replied_to', 1, 0)
+    
+    # Choose Unique business names
+    
+    business_names = list(df['business_name'].unique())
+    
+    # initialize final dataframe 
+    
+    df_final = pd.DataFrame()
+        
+        # Iterate by business to get unigrams and corresponding score
+        
+    for i in range(0,len(business_names)):
+        
+        # dynamically filter to business
+        
+        dfx = df[df['business_name'].str.contains(business_names[i])].reset_index() 
+        
+        print("The length of " + str(dfx['business_name'].unique()) + " is " + str(len(dfx)))
+    
+        # tokenize each tweet
+        
+        unigrams = list(dfx.apply(lambda row: get_text(row['tweet_message']), axis=1))
+        
+        # put unigrams in their own column
+        
+        dfx['unigrams'] = unigrams
+        
+        # create a list of bigrams given unigram list (renamed function so i could keep object naming consistent)
+        
+        bigrams = [list(bi_grams(x)) for x in unigrams]
+        
+        # turn bigrams into something more readable
+        
+        bigrams = [[("%s "*len(x)%x).strip() for x in y] for y in bigrams]
+        
+        # put bigrams in their own column
+        
+        dfx['bigrams'] = bigrams
+        
+        # place into each into their own columns
+        
+        dfx = pd.concat([dfx['conversations_status'],pd.DataFrame(dfx.bigrams.tolist())],1)
+        
+        # isolate column names without conversion status
+        
+        col_names = dfx.columns[1:]
+        
+        # melt dataframe to just words and statuses
+        
+        dfx = pd.melt(dfx, id_vars = 'conversations_status', value_name = 'bigrams', value_vars = col_names).reset_index()
+        
+        # keep just what we want and remove None values
+        
+        dfx = dfx[['bigrams','conversations_status']].dropna().reset_index()
+            
+        # group by unigrams, get average conversion status and total count
+        
+        dfx = dfx.groupby('bigrams')['conversations_status'].agg(['mean', 'count']).reset_index()
+        
+        dfx.columns = ['bigrams','value','count']
+        
+        dfx['business_name'] = business_names[i]
+        
+        dfx = dfx[dfx['count'] >= 5].sort_values(by=['count','value'],ascending = False)
+        
+        print(str(business_names[i]) + " is completed")
+        
+        df_final = df_final.append(dfx)
+        
+        print(dfx.head())
+        
+    return df_final
 
-#def bigram_vals(tweet_df):
+############################ Trigram 
 
-############################
-
-# Get Trigram Values by client for each 
-
-#def trigram_vals(tweet_df):
-
-############################
+def trigram_vals(tweet_df):
+        
+    '''prerequisite functions: get_text'''
+    
+    # Read in just what we want
+    
+    df = tweet_df.loc[:,['business_name','conversations_status','tweet_message']]
+    
+    # Convert conversations status to binary
+    
+    df['conversations_status'] = np.where(df['conversations_status'] == 'replied_to', 1, 0)
+    
+    # Choose Unique business names
+    
+    business_names = list(df['business_name'].unique())
+    
+    # initialize final dataframe 
+    
+    df_final = pd.DataFrame()
+        
+        # Iterate by business to get unigrams and corresponding score
+        
+    for i in range(0,len(business_names)):
+        
+        # dynamically filter to business
+        
+        dfx = df[df['business_name'].str.contains(business_names[i])].reset_index()
+    
+        print("The length of " + str(dfx['business_name'].unique()) + " is " + str(len(dfx)))
+    
+        # tokenize each tweet
+        
+        unigrams = list(dfx.apply(lambda row: get_text(row['tweet_message']), axis=1))
+        
+        # put unigrams in their own column
+        
+        dfx['unigrams'] = unigrams
+        
+        # create a list of trigrams given unigram list (renamed function so i could keep object naming consistent)
+        
+        trigrams = [list(tri_grams(x)) for x in unigrams]
+        
+        # turn trigrams into something more readable
+        
+        trigrams = [[("%s "*len(x)%x).strip() for x in y] for y in trigrams]
+        
+        # put trigrams in their own column
+        
+        dfx['trigrams'] = trigrams
+        
+        # place into each into their own columns
+        
+        dfx = pd.concat([dfx['conversations_status'],pd.DataFrame(dfx.trigrams.tolist())],1)
+        
+        # isolate column names without conversion status
+        
+        col_names = dfx.columns[1:]
+        
+        # melt dataframe to just words and statuses
+        
+        dfx = pd.melt(dfx, id_vars = 'conversations_status', value_name = 'trigrams', value_vars = col_names).reset_index()
+        
+        # keep just what we want and remove None values
+        
+        dfx = dfx[['trigrams','conversations_status']].dropna().reset_index()
+            
+        # group by unigrams, get average conversion status and total count
+        
+        dfx = dfx.groupby('trigrams')['conversations_status'].agg(['mean', 'count']).reset_index()
+        
+        dfx.columns = ['trigrams','value','count']
+        
+        dfx['business_name'] = business_names[i]
+        
+        dfx = dfx[dfx['count'] >= 5].sort_values(by=['count','value'],ascending = False)
+        
+        print(str(business_names[i]) + " is completed")
+        
+        df_final = df_final.append(dfx)
+        
+        print(dfx.head())
+        
+    return df_final
+############################ Engine
+    
 '''
 def Engine(tweet_df):
     
--- Natural Language Processing/TweetValuationEnv.py
+++ Natural Language Processing/TweetValuationEnv.py
@@ -14,6 +14,20 @@ from TweetValuation import *
 
 tweet_df = pd.read_csv('TweetResponseSample.csv')
 
+# Unigrams
+
 unigram_vals = unigram_vals(tweet_df)
 
 unigram_vals.to_csv('unigram_vals.csv')
+
+# Bigrams
+
+bigram_vals = bigram_vals(tweet_df)
+
+bigram_vals.to_csv('bigram_vals.csv')
+
+# Trigrams 
+
+trigram_vals = trigram_vals(tweet_df)
+
+trigram_vals.to_csv('trigram_vals.csv')
